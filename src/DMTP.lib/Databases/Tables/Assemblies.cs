90e96bb9a89c374408b8146af438b6ebd0520267
Jarred Capellman <jcapellman@hotmail.com>
2019-07-03 21:28:00 -0500

    Change: Updated the Assemblies class to inherit from BaseTable

--- src/DMTP.lib/Databases/Tables/Assemblies.cs
+++ src/DMTP.lib/Databases/Tables/Assemblies.cs
@@ -1,13 +1,9 @@
-﻿using System;
-
-using DMTP.lib.Databases.Tables.Base;
+﻿using DMTP.lib.Databases.Tables.Base;
 
 namespace DMTP.lib.Databases.Tables
 {
     public class Assemblies : BaseTable
     {
-        public Guid ID { get; set; }
-
         public string Name { get; set; }
 
         public byte[] Data { get; set; }
