deebc237ac5d72185f7a8e9fb7ef3236f266b8d2
Jarred Capellman <jcapellman@hotmail.com>
2019-07-07 09:11:02 -0500

    Added: IsValid method to the BaseTable Class for property validation

--- src/DMTP.lib/Databases/Tables/Base/BaseTable.cs
+++ src/DMTP.lib/Databases/Tables/Base/BaseTable.cs
@@ -1,9 +1,35 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace DMTP.lib.Databases.Tables.Base
 {
     public class BaseTable
     {
         public Guid ID { get; set; }
+
+        public bool IsValid()
+        {
+            var properties = GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var property in properties)
+            {
+                var requiredAttribute = property.GetCustomAttribute<RequiredAttribute>();
+
+                if (requiredAttribute == null)
+                {
+                    continue;
+                }
+
+                if (property.GetValue(this) != null)
+                {
+                    continue;
+                }
+
+                return false;
+            }
+
+            return true;
+        }
     }
 }
