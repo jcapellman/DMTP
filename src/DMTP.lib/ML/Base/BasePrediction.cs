fdbfa39ad3492c36c86767f3f4d148c3b768c37f
Jarred Capellman <jcapellman@hotmail.com>
2019-07-15 19:49:19 -0500

    Change: Updated the NuGet packages and further enhanced the error handling

--- src/DMTP.lib/ML/Base/BasePrediction.cs
+++ src/DMTP.lib/ML/Base/BasePrediction.cs
@@ -9,10 +9,14 @@ using DMTP.lib.Options;
 
 using Microsoft.ML;
 
+using NLog;
+
 namespace DMTP.lib.ML.Base
 {
     public abstract class BasePrediction
     {
+        private static Logger Log = NLog.LogManager.GetCurrentClassLogger();
+
         public abstract string MODEL_NAME { get; }
 
         private const int FILE_ENCODING = 1252;
@@ -32,37 +36,44 @@ namespace DMTP.lib.ML.Base
 
         protected string GetStrings(byte[] data, int start, int length)
         {
-            var stringLines = new StringBuilder();
+            try {
+                var stringLines = new StringBuilder();
 
-            if (data == null || data.Length == 0)
-            {
-                return stringLines.ToString();
-            }
-
-            var dataToParse = data.Length < data.Length - length - start ? data : data.Skip(start).Take(length).ToArray();
-
-            using (var ms = new MemoryStream(dataToParse, false))
-            {
-                using (var streamReader = new StreamReader(ms, Encoding.GetEncoding(FILE_ENCODING), false, BUFFER_SIZE, false))
+                if (data == null || data.Length == 0)
                 {
-                    while (!streamReader.EndOfStream)
+                    return stringLines.ToString();
+                }
+
+                var dataToParse = data.Length < data.Length - length - start ? data : data.Skip(start).Take(length).ToArray();
+
+                using (var ms = new MemoryStream(dataToParse, false))
+                {
+                    using (var streamReader = new StreamReader(ms, Encoding.GetEncoding(FILE_ENCODING), false, BUFFER_SIZE, false))
                     {
-                        var line = streamReader.ReadLine();
-
-                        if (string.IsNullOrEmpty(line))
+                        while (!streamReader.EndOfStream)
                         {
-                            continue;
+                            var line = streamReader.ReadLine();
+
+                            if (string.IsNullOrEmpty(line))
+                            {
+                                continue;
+                            }
+
+                            line = line.Replace("^", "").Replace(")", "").Replace("-", "");
+
+                            stringLines.Append(string.Join(string.Empty,
+                                _stringRex.Matches(line).Where(a => !string.IsNullOrEmpty(a.Value) && !string.IsNullOrWhiteSpace(a.Value)).ToList()));
                         }
-
-                        line = line.Replace("^", "").Replace(")", "").Replace("-", "");
-
-                        stringLines.Append(string.Join(string.Empty,
-                            _stringRex.Matches(line).Where(a => !string.IsNullOrEmpty(a.Value) && !string.IsNullOrWhiteSpace(a.Value)).ToList()));
                     }
                 }
-            }
 
-            return string.Join(string.Empty, stringLines);
+                return string.Join(string.Empty, stringLines);
+            } catch (Exception ex)
+            {
+                Log.Error($"Failure in Getting Strings for {MODEL_NAME} due to {ex}");
+
+                return null;
+            }
         }
 
         protected string OutputModelPath =>
@@ -70,12 +81,21 @@ namespace DMTP.lib.ML.Base
 
         protected string SaveModel(ITransformer trainedModel, DataViewSchema schema, TrainerCommandLineOptions options)
         {
-            using (var fileStream = new FileStream(OutputModelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+            try
             {
-                MlContext.Model.Save(trainedModel, schema, fileStream);
-            }
+                using (var fileStream = new FileStream(OutputModelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+                {
+                    MlContext.Model.Save(trainedModel, schema, fileStream);
+                }
 
-            return OutputModelPath;
+                return OutputModelPath;
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Failure in saving model to {OutputModelPath} due to {ex}");
+
+                return null;
+            }
         }
 
         public abstract (T Data, string Output) FeatureExtraction<T>(ClassifierResponseItem response)
