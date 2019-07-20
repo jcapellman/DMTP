5bbf2d062399847104a909fc249e6d3a47cb6bca
Jarred Capellman <jcapellman@hotmail.com>
2019-07-20 12:07:51 -0500

    Change: Finished cleaning up the Uploader Program class

--- src/DMTP.Uploader/Program.cs
+++ src/DMTP.Uploader/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+
 using DMTP.lib.Databases.Tables;
 using DMTP.lib.Enums;
 using DMTP.lib.Handlers;
@@ -8,43 +10,47 @@ namespace DMTP.Uploader
 {
     class Program
     {
-        private static (string Url, Jobs Job) ParseCommandLineArgs(string[] args)
+        private const int NUM_REQUIRED_ARGUMENTS = 4;
+
+        private static (string Url, Jobs Job) ParseCommandLineArgs(IReadOnlyList<string> args)
         {
             // Eventually force the use of a json file
-            if (args.Length < 4)
+            if (args.Count != NUM_REQUIRED_ARGUMENTS)
             {
-                Console.WriteLine($"Usage is:{Environment.NewLine}FileClassifier.JobManager.Uploader <name> <model type> <path to data> <server url>");
+                Console.WriteLine($"Usage is:{Environment.NewLine}DMTP.Uploader <name> <model type> <path to data> <server url>");
 
                 return (null, null);
             }
 
-            if (!Enum.TryParse<ModelType>(args[1], true, out _))
+            if (Enum.TryParse<ModelType>(args[1], true, out _))
             {
-                Console.WriteLine($"Invalid Model Type option ({args[1]})");
-
-                return (null, null);
+                return (args[3], new Jobs
+                {
+                    ModelType = args[1],
+                    TrainingDataPath = args[2],
+                    Name = args[0]
+                });
             }
 
-            return (args[3], new Jobs
-            {
-                ModelType = args[1],
-                TrainingDataPath = args[2],
-                Name = args[0]
-            });
+            Console.WriteLine($"Invalid Model Type option ({args[1]})");
+
+            return (null, null);
         }
 
         static async Task Main(string[] args)
         {
-            var (Url, Job) = ParseCommandLineArgs(args);
+            var (url, job) = ParseCommandLineArgs(args);
             
-            if (Job == null || Url == null)
+            if (job == null || url == null)
             {
+                Console.WriteLine("Failed to parse arguments - exiting");
+
                 return;
             }
 
-            var jobHandler = new JobHandler(Url);
+            var jobHandler = new JobHandler(url);
 
-            var result = await jobHandler.AddNewJobAsync(Job);
+            var result = await jobHandler.AddNewJobAsync(job);
 
             Console.WriteLine(result ? "Job successfully uploaded" : "Failed to upload job");
         }
