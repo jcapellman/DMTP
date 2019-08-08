5309b1f7e2004f697313085774ec6040decc7443
Jarred Capellman <jcapellman@hotmail.com>
2019-08-08 10:00:00 -0500

    Added: Initial work on the Login Form

--- src/DMTP.REST/Controllers/AccountController.cs
+++ src/DMTP.REST/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DMTP.REST.Models;
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DMTP.REST.Controllers
@@ -9,6 +11,17 @@ namespace DMTP.REST.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AttemptLogin(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
