8c6f898b1e77af1d4b71fac4933c81b4e5410c5d
Jarred Capellman <jcapellman@hotmail.com>
2019-08-10 09:40:56 -0500

    Added: Initial work on the Users Table

---
+++ src/DMTP.lib/Databases/Tables/Users.cs
@@ -0,0 +1,11 @@
+﻿using DMTP.lib.Databases.Tables.Base;
+
+namespace DMTP.lib.Databases.Tables
+{
+    public class Users : BaseTable
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
