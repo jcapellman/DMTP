a354d36bc650eef5de627a38fcf8b0cfb0b6ae83
Jarred Capellman <jcapellman@hotmail.com>
2019-08-10 11:25:29 -0500

    Added: CreateUserModel class

---
+++ src/DMTP.REST/Models/CreateUserModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DMTP.REST.Models
+{
+    public class CreateUserModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
