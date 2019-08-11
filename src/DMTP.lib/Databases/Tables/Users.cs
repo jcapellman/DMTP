f3249fa4ab3476f51e4eb7ec795c866aac469dba
Jarred Capellman <jcapellman@hotmail.com>
2019-08-11 13:38:57 -0500

    Added: Active property to the Users table

--- src/DMTP.lib/Databases/Tables/Users.cs
+++ src/DMTP.lib/Databases/Tables/Users.cs
@@ -7,5 +7,7 @@ namespace DMTP.lib.Databases.Tables
         public string Username { get; set; }
 
         public string Password { get; set; }
+
+        public bool Active { get; set; }
     }
 }
