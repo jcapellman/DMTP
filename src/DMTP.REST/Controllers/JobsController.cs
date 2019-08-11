2b2a11dee975d77d8644cda69d0c6c3cc180a03f
Jarred Capellman <jcapellman@hotmail.com>
2019-08-10 20:32:16 -0500

    Added: Initial work on the JobsController

---
+++ src/DMTP.REST/Controllers/JobsController.cs
@@ -0,0 +1,17 @@
+﻿using DMTP.lib.Databases.Base;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DMTP.REST.Controllers
+{
+    [Authorize]
+    public class JobsController : BaseController
+    {
+        public JobsController(IDatabase database) : base(database)
+        {
+        }
+
+        public IActionResult Index() => View(Database.GetJobs());
+    }
+}
