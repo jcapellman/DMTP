6a38deb9d2911a0925831f62ee4e56edabd18db8
Jarred Capellman <jcapellman@hotmail.com>
2019-08-11 09:02:34 -0500

    Added: Initial work on the UsersController

---
+++ src/DMTP.REST/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using DMTP.lib.Databases.Base;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace DMTP.REST.Controllers
+{
+    public class UsersController : BaseController
+    {
+        public UsersController(IDatabase database) : base(database)
+        {
+        }
+
+        public IActionResult Index() => View(Database.GetUsers());
+    }
+}
