6bc9bf76e96fd8d1a17c34e32f3bd46e80a9a786
Jarred Capellman <jcapellman@hotmail.com>
2019-08-11 12:40:13 -0500

    Added: UserLogins Table

---
+++ src/DMTP.lib/Databases/Tables/UserLogins.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using DMTP.lib.Databases.Tables.Base;
+
+namespace DMTP.lib.Databases.Tables
+{
+    public class UserLogins : BaseTable
+    {
+        public Guid? UserID { get; set; }
+        
+        public DateTimeOffset Timestamp { get; set; }
+
+        public string Username { get; set; }
+
+        public bool Successful { get; set; }
+
+        public string IPAddress { get; set; }
+    }
+}
