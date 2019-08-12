8f011ab9c2102936446c2f035c1d41f9ac867813
Jarred Capellman <jcapellman@hotmail.com>
2019-08-11 20:30:34 -0500

    Added: Initial work on the Settings Table

---
+++ src/DMTP.lib/Databases/Tables/Settings.cs
@@ -0,0 +1,17 @@
+﻿using DMTP.lib.Databases.Tables.Base;
+
+namespace DMTP.lib.Databases.Tables
+{
+    public class Settings : BaseTable
+    {
+        public string SMTPHostName { get; set; }
+
+        public int SMTPPortNumber { get; set; }
+
+        public string SMTPUsername { get; set; }
+
+        public string SMTPPassword { get; set; }
+
+        public bool AllowNewUserCreation { get; set; }
+    }
+}
