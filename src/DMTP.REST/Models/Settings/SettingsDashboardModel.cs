32e460ddc5b5fee5d700d22aaa0a01795aa1bdae
Jarred Capellman <jcapellman@hotmail.com>
2019-08-11 20:29:43 -0500

    Added: Initial work on the SettingsDashboardModel class

---
+++ src/DMTP.REST/Models/Settings/SettingsDashboardModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DMTP.REST.Models.Settings
+{
+    public class SettingsDashboardModel
+    {
+        public lib.Databases.Tables.Settings Setting { get; set; }
+
+        public string ActionMessage { get; set; }
+    }
+}
