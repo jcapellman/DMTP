d1e6978610af7b9d4f38ec0d959cda2ccebe6998
Jarred Capellman <jcapellman@hotmail.com>
2019-08-11 19:41:31 -0500

    Added: Initial commit of EditUserModel class

---
+++ src/DMTP.REST/Models/Users/EditUserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DMTP.REST.Models.Users
+{
+    public class EditUserModel
+    {
+        public Guid ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Message { get; set; }
+    }
+}
