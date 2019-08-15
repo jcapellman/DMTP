cab6fe0ea95069b542fefa1ff862077108fca549
Jarred Capellman <jcapellman@hotmail.com>
2019-08-14 21:46:02 -0500

    Added: SENDER_EMAIL constant in the Constants class

--- src/DMTP.lib/Common/Constants.cs
+++ src/DMTP.lib/Common/Constants.cs
@@ -11,5 +11,7 @@
         public const string MODEL_EXTENSION = "mdl";
 
         public const string ASSEMBLY_EXTENSION = "dasm";
+
+        public const string SENDER_EMAIL = "no-reply@dmtp";
     }
 }
