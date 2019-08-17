491f3a93f09d0af9dbc61ef6a2503f9ec053b970
Jarred Capellman <jcapellman@hotmail.com>
2019-08-17 11:40:04 -0500

    Added: Initial work on the RoleDashboardModel class

---
+++ src/DMTP.REST/Models/Roles/RoleDashboardModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DMTP.REST.Models.Roles
+{
+    public class RoleDashboardModel
+    {
+        public List<RoleListingItem> RolesListing { get; set; }
+
+        public string ActionMessage { get; set; }
+    }
+}
