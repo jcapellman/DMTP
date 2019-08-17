58c0aa78ef26a8f952155cf939cc2e11d69ea935
Jarred Capellman <jcapellman@hotmail.com>
2019-08-17 18:58:47 -0500

    Added: Initial work on the BaseModel class

---
+++ src/DMTP.REST/Models/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using DMTP.REST.Auth;
+
+namespace DMTP.REST.Models
+{
+    public class BaseModel
+    {
+        public ApplicationUser CurrentUser;
+
+        public BaseModel(ApplicationUser user)
+        {
+            CurrentUser = user;
+        }
+    }
+}
