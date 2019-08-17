cde42c1e41c21d902c0d66f0bacef7780931aed8
Jarred Capellman <jcapellman@hotmail.com>
2019-08-17 11:40:32 -0500

    Added: Initial work on the RoleListingItem class

---
+++ src/DMTP.REST/Models/Roles/RoleListingItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DMTP.REST.Models.Roles
+{
+    public class RoleListingItem
+    {
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int NumberAssociatedUsers { get; set; }
+
+        public bool IsBuiltIn { get; set; }
+    }
+}
