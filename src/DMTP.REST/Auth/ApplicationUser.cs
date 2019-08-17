ff452026cc54ea40a2f63407d97f876a1cd6f7ca
Jarred Capellman <jcapellman@hotmail.com>
2019-08-17 18:34:13 -0500

    Added: Initial work on the ApplicationUser class

---
+++ src/DMTP.REST/Auth/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using DMTP.lib.Databases.Tables;
+
+namespace DMTP.REST.Auth
+{
+    public class ApplicationUser
+    {
+        public Guid ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Roles Role { get; set; }
+    }
+}
