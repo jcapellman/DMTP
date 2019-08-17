aebf5a41dc79a2f08982baf256b4be4eb1760084
Jarred Capellman <jcapellman@hotmail.com>
2019-08-17 18:34:52 -0500

    Added: ApplicationUser to the RoleDashboardModel class

--- src/DMTP.REST/Models/Roles/RoleDashboardModel.cs
+++ src/DMTP.REST/Models/Roles/RoleDashboardModel.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections.Generic;
 
+using DMTP.REST.Auth;
+
 namespace DMTP.REST.Models.Roles
 {
     public class RoleDashboardModel
     {
+        public RoleDashboardModel(ApplicationUser user)
+        {
+            CurrentUser = user;
+        }
+
+        public ApplicationUser CurrentUser { get; set; }
+
         public List<RoleListingItem> RolesListing { get; set; }
 
         public string ActionMessage { get; set; }
