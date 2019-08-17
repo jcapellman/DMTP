60ab4390cec8e64c1901808cc8524644e9126044
Jarred Capellman <jcapellman@hotmail.com>
2019-08-17 18:58:21 -0500

    Change: UserDashboardModel class inherits from BaseModel class

--- src/DMTP.REST/Models/Users/UserDashboardModel.cs
+++ src/DMTP.REST/Models/Users/UserDashboardModel.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections.Generic;
 
 using DMTP.lib.Databases.Tables;
+using DMTP.REST.Auth;
 
 namespace DMTP.REST.Models.Users
 {
-    public class UserDashboardModel
+    public class UserDashboardModel : BaseModel
     {
         public List<UserListingItem> UsersListing { get; set; }
 
         public List<UserLogins> UserLoginListing { get; set; }
 
         public string ActionMessage { get; set; }
+
+        public UserDashboardModel(ApplicationUser user) : base(user)
+        {
+        }
     }
 }
