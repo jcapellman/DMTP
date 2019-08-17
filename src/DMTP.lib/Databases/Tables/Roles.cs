da2fe9f5e345a0ac38d24cf7670894792145ad9e
Jarred Capellman <jcapellman@hotmail.com>
2019-08-17 18:39:12 -0500

    Added: Security Properties to the Roles class

--- src/DMTP.lib/Databases/Tables/Roles.cs
+++ src/DMTP.lib/Databases/Tables/Roles.cs
@@ -6,10 +6,28 @@ namespace DMTP.lib.Databases.Tables
     {
         public string Name { get; set; }
 
+        public bool Active { get; set; }
+
         public bool BuiltIn { get; set; }
 
-        public bool CanEditUsers { get; set; }
+        public bool UsersEdit { get; set; }
 
-        public bool CanEditSettings { get; set; }
+        public bool UsersView { get; set; }
+
+        public bool UsersDelete { get; set; }
+
+        public bool SettingsEdit { get; set; }
+
+        public bool SettingsView { get; set; }
+
+        public bool CanUploadModels { get; set; }
+
+        public bool CanDownloadModels { get; set; }
+
+        public bool RolesDelete { get; set; }
+
+        public bool RolesEdit { get; set; }
+
+        public bool RolesView { get; set; }
     }
 }
