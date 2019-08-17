7838b3bffc3114e0085715098b0b5a0ac997cb13
Jarred Capellman <jcapellman@hotmail.com>
2019-08-17 11:41:35 -0500

    Added: Initial work on the Roles Table class

---
+++ src/DMTP.lib/Databases/Tables/Roles.cs
@@ -0,0 +1,15 @@
+﻿using DMTP.lib.Databases.Tables.Base;
+
+namespace DMTP.lib.Databases.Tables
+{
+    public class Roles : BaseTable
+    {
+        public string Name { get; set; }
+
+        public bool BuiltIn { get; set; }
+
+        public bool CanEditUsers { get; set; }
+
+        public bool CanEditSettings { get; set; }
+    }
+}
