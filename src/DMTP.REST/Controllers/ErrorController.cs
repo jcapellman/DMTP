44760a797f725676f4b0e874488551300e3d1fdb
Jarred Capellman <jcapellman@hotmail.com>
2019-08-18 09:10:23 -0500

    Added: AccessSections implementation in the ErrorController

--- src/DMTP.REST/Controllers/ErrorController.cs
+++ src/DMTP.REST/Controllers/ErrorController.cs
@@ -1,5 +1,6 @@
 ﻿using DMTP.lib.Databases.Base;
 using DMTP.lib.Databases.Tables;
+using DMTP.lib.Enums;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,8 @@ namespace DMTP.REST.Controllers
     [Authorize]
     public class ErrorController : BaseController
     {
+        protected override AccessSections CurrentSection => AccessSections.ERROR;
+
         public ErrorController(IDatabase database, Settings settings) : base(database, settings)
         {
         }
