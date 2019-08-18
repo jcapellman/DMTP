d2955abcef6bb6ce03572a1679dc1eacb67d793e
Jarred Capellman <jcapellman@hotmail.com>
2019-08-18 09:10:56 -0500

    Added: AccessSections implementation in the JobsController

--- src/DMTP.REST/Controllers/JobsController.cs
+++ src/DMTP.REST/Controllers/JobsController.cs
@@ -1,5 +1,6 @@
 ﻿using DMTP.lib.Databases.Base;
 using DMTP.lib.Databases.Tables;
+using DMTP.lib.Enums;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,8 @@ namespace DMTP.REST.Controllers
     [Authorize]
     public class JobsController : BaseController
     {
+        protected override AccessSections CurrentSection => AccessSections.JOBS;
+
         public JobsController(IDatabase database, Settings settings) : base(database, settings)
         {
         }
