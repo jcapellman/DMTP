74148c8813eba2e5be6fe14f63b7da866f429188
Jarred Capellman <jcapellman@hotmail.com>
2019-08-18 09:21:34 -0500

    Added: AccessSections implementation in the BaseAPIController

--- src/DMTP.REST/Controllers/BaseAPIController.cs
+++ src/DMTP.REST/Controllers/BaseAPIController.cs
@@ -1,5 +1,6 @@
 ﻿using DMTP.lib.Databases.Base;
 using DMTP.lib.Databases.Tables;
+using DMTP.lib.Enums;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,8 @@ namespace DMTP.REST.Controllers
     [Authorize]
     public class BaseAPIController : BaseController
     {
+        protected override AccessSections CurrentSection => AccessSections.API;
+
         protected BaseAPIController(IDatabase database, Settings settings) : base(database, settings)
         {
         }
