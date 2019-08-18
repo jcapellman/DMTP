6b475e6e13b5e2a61d7642aa2e2c219b8d9dd564
Jarred Capellman <jcapellman@hotmail.com>
2019-08-17 20:07:41 -0500

    Added: Initial work on the SetupModel class

---
+++ src/DMTP.REST/Models/Setup/SetupModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DMTP.REST.Models.Setup
+{
+    public class SetupModel
+    {
+        public string ActionMessage { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string SMTPHostName { get; set; }
+
+        [Required]
+        public string SMTPUsername { get; set; }
+
+        [Required]
+        public string SMTPPassword { get; set; }
+
+        [Required]
+        public int SMTPPortNumber { get; set; }
+
+        [Required]
+        public bool AllowUserCreation { get; set; }
+    }
+}
