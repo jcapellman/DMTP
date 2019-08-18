976edc2cda7dea9c530d390e00c1a1ac5012b554
Jarred Capellman <jcapellman@hotmail.com>
2019-08-18 09:21:17 -0500

    Added: AccessSections implementation in the SetupController

--- src/DMTP.REST/Controllers/SetupController.cs
+++ src/DMTP.REST/Controllers/SetupController.cs
@@ -1,5 +1,6 @@
 ﻿using DMTP.lib.Databases.Base;
 using DMTP.lib.Databases.Tables;
+using DMTP.lib.Enums;
 using DMTP.REST.Models.Setup;
 
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +9,8 @@ namespace DMTP.REST.Controllers
 {
     public class SetupController : BaseController
     {
+        protected override AccessSections CurrentSection => AccessSections.SETUP;
+
         public SetupController(IDatabase database, Settings settings) : base(database, settings)
         {
         }
