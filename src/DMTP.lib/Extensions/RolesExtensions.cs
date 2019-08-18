8d47155c15bf2ff9e3fee16a9b1a277d376db731
Jarred Capellman <jcapellman@hotmail.com>
2019-08-18 09:08:57 -0500

    Added: RolesExtensions Extension class

---
+++ src/DMTP.lib/Extensions/RolesExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+using DMTP.lib.Databases.Tables;
+using DMTP.lib.Enums;
+
+namespace DMTP.lib.Extensions
+{
+    public static class RolesExtensions
+    {
+        public static bool HasPermissions(this Roles role, AccessSections section, AccessLevels level) => role.Permissions.Any(a => a.Key == section && a.Value >= level);
+    }
+}
