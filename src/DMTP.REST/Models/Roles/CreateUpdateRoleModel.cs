87680b86b55adcc59287359b91f9f0efa7a9e676
Jarred Capellman <jcapellman@hotmail.com>
2019-08-22 19:26:44 -0500

    Added: Initial work on the CreateUpdateRoleModel #10

---
+++ src/DMTP.REST/Models/Roles/CreateUpdateRoleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using DMTP.lib.Enums;
+
+namespace DMTP.REST.Models.Roles
+{
+    public class CreateUpdateRoleModel
+    {
+        public Guid? ID { get; set; }
+
+        public string ActionMessage { get; set; }
+
+        public string Name { get; set; }
+
+        public Dictionary<AccessSections, AccessLevels> Permissions { get; set; }
+    }
+}
