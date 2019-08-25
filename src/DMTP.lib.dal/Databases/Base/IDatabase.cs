9a1f78c11d94cbde7631a7e536f3ced499385331
Jarred Capellman <jcapellman@hotmail.com>
2019-08-24 19:51:31 -0500

    Change: Refactored the IDatabase and LiteDBDatabase to use a generic interface #17

---
+++ src/DMTP.lib.dal/Databases/Base/IDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using DMTP.lib.dal.Databases.Tables.Base;
+
+namespace DMTP.lib.dal.Databases.Base
+{
+    public interface IDatabase
+    {
+        Guid Insert<T>(T objectValue) where T : BaseTable;
+
+        bool DeleteAll<T>() where T : BaseTable;
+
+        bool Delete<T>(Guid id) where T : BaseTable;
+
+        T GetOneById<T>(Guid id) where T : BaseTable;
+
+        T GetOne<T>(System.Linq.Expressions.Expression<Func<T, bool>> query) where T : BaseTable;
+
+        List<T> GetAll<T>() where T : BaseTable;
+
+        bool Update<T>(T objectValue) where T : BaseTable;
+    }
+}
---
+++ src/DMTP.lib.dal/Databases/LiteDBDatabase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+using DMTP.lib.dal.Databases.Base;
+using DMTP.lib.dal.Databases.Tables.Base;
+
+using LiteDB;
+
+namespace DMTP.lib.dal.Databases
+{
+    public class LiteDBDatabase : IDatabase
+    {
+        private readonly NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
+
+        private const string DbFilename = "data.db";
+
+        public Guid Insert<T>(T objectValue) where T : BaseTable
+        {
+            using (var db = new LiteDatabase(DbFilename))
+            {
+                objectValue.Active = true;
+
+                return db.GetCollection<T>().Insert(objectValue);
+            }
+        }
+
+        public bool DeleteAll<T>() where T : BaseTable
+        {
+            using (var db = new LiteDatabase(DbFilename))
+            {
+                return db.DropCollection(nameof(T));
+            }
+        }
+
+        public bool Delete<T>(Guid id) where T : BaseTable
+        {
+            using (var db = new LiteDatabase(DbFilename))
+            {
+                return db.GetCollection<T>().Delete(id);
+            }
+        }
+
+        public T GetOneById<T>(Guid id) where T : BaseTable
+        {
+            using (var db = new LiteDatabase(DbFilename))
+            {
+                return db.GetCollection<T>().FindOne(a => a.ID == id && a.Active);
+            }
+        }
+
+        public T GetOne<T>(Expression<Func<T, bool>> query) where T : BaseTable
+        {
+            using (var db = new LiteDatabase(DbFilename))
+            {
+                return db.GetCollection<T>().FindOne(query);
+            }
+        }
+
+        public List<T> GetAll<T>() where T : BaseTable
+        {
+            using (var db = new LiteDatabase(DbFilename))
+            {
+                return db.GetCollection<T>().Find(a => a.Active).ToList();
+            }
+        }
+
+        public bool Update<T>(T objectValue) where T : BaseTable
+        {
+            using (var db = new LiteDatabase(DbFilename))
+            {
+                return db.GetCollection<T>().Update(objectValue);
+            }
+        }
+    }
+}
