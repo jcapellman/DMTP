93633ad8e909c7fa8d670db524cb01b4a143a19c
Jarred Capellman <jcapellman@hotmail.com>
2019-08-30 20:30:08 -0500

    Added: GenerateRandomString method to populate the DeviceKeyPassword

---
+++ src/DMTP.lib/Helpers/Strings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace DMTP.lib.Helpers
+{
+    public static class Strings
+    {
+        public static string GenerateRandomString(int length = 20)
+        {
+            var random = new Random((int)DateTime.Now.Ticks);
+
+            return new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
