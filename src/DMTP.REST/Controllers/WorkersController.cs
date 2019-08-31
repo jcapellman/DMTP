4be4ddd7a77c5f2618325fa54b7d98a1dce03b1a
Jarred Capellman <jcapellman@hotmail.com>
2019-08-30 20:33:39 -0500

    Added: ComputeRegistrationKey method and populating the Index view

--- src/DMTP.REST/Controllers/WorkersController.cs
+++ src/DMTP.REST/Controllers/WorkersController.cs
@@ -1,6 +1,9 @@
 ﻿using DMTP.lib.dal.Databases.Tables;
 using DMTP.lib.dal.Manager;
 using DMTP.lib.Managers;
+using DMTP.lib.Security;
+
+using DMTP.REST.Models.Workers;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +13,16 @@ namespace DMTP.REST.Controllers
     [Authorize]
     public class WorkersController : BaseController
     {
+        private string ComputeRegistrationKey() => AES.EncryptString($"{HttpContext.Request.Host.Host}", CurrentSettings.DeviceKeyPassword);
+
         public WorkersController(DatabaseManager database, Settings settings) : base(database, settings)
         {
         }
 
-        public IActionResult Index() => View(new WorkerManager(Database).GetWorkers());
+        public IActionResult Index() => View(new WorkerListingModel
+        {
+            WorkersListing = new WorkerManager(Database).GetWorkers(),
+            RegistrationKey = ComputeRegistrationKey()
+        });
     }
 }
