f8d012413b3e0d2732ca9166104d040b51208362
Jarred Capellman <jcapellman@hotmail.com>
2019-08-30 20:31:14 -0500

    Added: WorkerListingModel to be used on the Index View

---
+++ src/DMTP.REST/Models/Workers/WorkerListingModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DMTP.REST.Models.Workers
+{
+    public class WorkerListingModel
+    {
+        public List<lib.dal.Databases.Tables.Workers> WorkersListing { get; set; }
+
+        public string RegistrationKey { get; set; }
+    }
+}
