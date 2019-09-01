ac6df2d4ed6e3e77efc8303ed38616f1e8f15bfe
Jarred Capellman <jcapellman@hotmail.com>
2019-09-01 10:24:06 -0500

    Added: NLog Logging in the Worker Program class

--- src/DMTP.Worker/Program.cs
+++ src/DMTP.Worker/Program.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 
 using DMTP.Worker.Common;
 using DMTP.Worker.Helpers;
 using DMTP.Worker.Objects;
 
+using NLog;
+
 namespace DMTP.Worker
 {
     class Program
     {
+        private static Logger Log = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
             var configExists = false;
@@ -17,18 +20,27 @@ namespace DMTP.Worker
 
             if (!File.Exists(Constants.CONFIG_FILE))
             {
-                Console.WriteLine($"{Constants.CONFIG_FILE} does not exist");
+                Log.Warn($"{Constants.CONFIG_FILE} does not exist, attempting to read command line arguments");
             }
             else
             {
                 configExists = true;
 
+                Log.Info($"{Constants.CONFIG_FILE} exists, attempting to read");
+
                 config = ConfigManager.ReadConfig(Constants.CONFIG_FILE);
+
+                if (config == null)
+                {
+                    Log.Error($"Failed to parse {Constants.CONFIG_FILE}, exiting");
+
+                    return;
+                }
             }
 
             if (args.Length != 2 && !configExists)
             {
-                Console.WriteLine($"{Constants.CONFIG_FILE} does not exist and not arguments passed in, exiting");
+                Log.Error($"{Constants.CONFIG_FILE} does not exist and no/invalid arguments passed in, exiting");
 
                 return;
             }
@@ -43,11 +55,13 @@ namespace DMTP.Worker
                 };
 
                 ConfigManager.WriteConfig(config, Constants.CONFIG_FILE);
+
+                Log.Debug($"{Constants.CONFIG_FILE} was initialized with the command line arguments and written to disk");
             }
 
             if (config == null)
             {
-                Console.WriteLine("Config was null, exiting");
+                Log.Error("Failed to initialize Config, exiting");
 
                 return;
             }
