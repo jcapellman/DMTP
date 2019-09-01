a4af04f073b3bd52af3ea4cd78b5b9918489cd89
Jarred Capellman <jcapellman@hotmail.com>
2019-09-01 11:08:50 -0500

    Added: Filter Based Authorization for the API

---
+++ src/DMTP.REST/Filters/APIAuthorize.cs
@@ -0,0 +1,32 @@
+﻿using DMTP.lib.dal.Databases.Tables;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DMTP.REST.Filters
+{
+    public class APIAuthorize : ActionFilterAttribute
+    {
+        private readonly Settings _settings;
+
+        public APIAuthorize(Settings settings)
+        {
+            _settings = settings;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue("Authorization", out var value))
+            {
+                context.Result = new ForbidResult();
+            }
+
+            if (value.ToString().Split(' ')[1] != _settings.DeviceKeyPassword)
+            {
+                context.Result = new ForbidResult();
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
