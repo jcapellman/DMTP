245c56524d7a88b7038dae481f9d3dd2da3ef191
Jarred Capellman <jcapellman@hotmail.com>
2019-09-02 15:37:00 -0500

    Added: Predictors and FileName property to the Assemblies

--- src/DMTP.lib.dal/Databases/Tables/Assemblies.cs
+++ src/DMTP.lib.dal/Databases/Tables/Assemblies.cs
@@ -1,10 +1,14 @@
-﻿using DMTP.lib.dal.Databases.Tables.Base;
+﻿using System.Collections.Generic;
+
+using DMTP.lib.dal.Databases.Tables.Base;
 
 namespace DMTP.lib.dal.Databases.Tables
 {
     public class Assemblies : BaseTable
     {
-        public string Name { get; set; }
+        public string FileName { get; set; }
+
+        public List<string> Predictors { get; set; }
 
         public byte[] Data { get; set; }
     }
