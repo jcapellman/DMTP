d9bb5104ad9a38f7ace7f57a3777d162d3d92a92
Jarred Capellman <jcapellman@hotmail.com>
2019-09-02 12:05:53 -0500

    Added: Initial work on the AssemblyDashboardModel class

---
+++ src/DMTP.REST/Models/Assemblies/AssemblyDashboardModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DMTP.REST.Models.Assemblies
+{
+    public class AssemblyDashboardModel
+    {
+        public List<lib.dal.Databases.Tables.Assemblies> AssembliesList { get; set; }
+
+        public string ActionMessage { get; set; }
+    }
+}
